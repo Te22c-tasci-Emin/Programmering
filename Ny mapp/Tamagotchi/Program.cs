195607164c62f61fb2bac6d872b7fccadf8c6251
Emin Tasci <Emin.tasci@elev.ga.ntig.se>
2024-09-12 11:18:38 +0200

    sifma

---
+++ Ny mapp/Tamagotchi/Program.cs
@@ -0,0 +1,11 @@
+﻿//objekt med egenskaper
+//metoder
+Weapon axe = new("Axe");
+
+food mat = new ("food");
+
+int damage = axe.Attack();
+
+Console.WriteLine(damage);
+
+Console.ReadLine();
---
+++ Ny mapp/Tamagotchi/Tamagotchi.cs
@@ -0,0 +1,19 @@
+
+public class Tamagotchi
+{
+    private int hunger;
+    private int boredom;
+    private List <string> words;
+
+    private bool isAlive = true;
+
+    public string Name;
+
+public void int Feed;
+
+
+
+}
+
+
+
